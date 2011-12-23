7ff4500b57c6ee47641f67b60668c62c12cbd26a
wapinet@gmail.com <wapinet@gmail.com@efb28ca5-a285-28e4-ec76-7e758185a0da>
2011-12-23 11:06:08 +0000

    fix XHTML errors

--- trunk/wap/header.php
+++ trunk/wap/header.php
@@ -82,30 +82,29 @@ $tpl_main = str_replace('<pun_page>', htmlspecialchars(basename($_SERVER['PHP_SE
 
 
 // START SUBST - <pun_title>
-//$tpl_main = str_replace('<pun_title>', '<div class="blocktable">'.pun_htmlspecialchars($pun_config['o_board_title']).'</div>', $tpl_main);
-$tpl_main = str_replace('<pun_title>', null, $tpl_main);
+//$tpl_main = str_replace('<pun_title>', '<div class="blocktable">' . pun_htmlspecialchars($pun_config['o_board_title']) . '</div>', $tpl_main);
+$tpl_main = str_replace('<pun_title>', '', $tpl_main);
 // END SUBST - <pun_title>
 
 $basename = basename($_SERVER['PHP_SELF']);
 
 // START SUBST - <pun_navlinks>
-//$tpl_main = str_replace('<pun_navlinks>','<div id="brdmenu" class="con"><a href="index.php">'.$lang_common['Index'].'</a></div>', $tpl_main);
-
+//$tpl_main = str_replace('<pun_navlinks>', '<div id="brdmenu" class="con"><a href="index.php">' . $lang_common['Index'] . '</a></div>', $tpl_main);
 //$tpl_main = str_replace('<pun_navlinks>', '', $tpl_main);
-
 // END SUBST - <pun_navlinks>
 
 //Logo на главной
 if ($basename == 'index.php') {
-    $tpl_main = str_replace('<pun_hd>', '<div class="hd"><img src="' . PUN_ROOT . 'style_wap/' . $pun_user['style_wap'] . '/logo.gif" title="' . $lang_common['Forum'] . ' ' . $_SERVER['HTTP_HOST'] . '" alt="' . $lang_common['Forum'] . ' ' . $_SERVER['HTTP_HOST'] . '"/></div>
-', $tpl_main);
+    $tpl_main = str_replace('<pun_hd>', '<div class="hd"><img src="' . PUN_ROOT . 'style_wap/' . $pun_user['style_wap'] . '/logo.gif" title="' . $lang_common['Forum'] . ' ' . $_SERVER['HTTP_HOST'] . '" alt="' . $lang_common['Forum'] . ' ' . $_SERVER['HTTP_HOST'] . '"/></div>', $tpl_main);
+} else {
+    $tpl_main = str_replace('<pun_hd>', '', $tpl_main);
 }
 
 // START SUBST - <pun_desc>//Описание форума на главной
-//FIXME: убрать вывод <div class="hd_bott"></div> если нет описания форума
-if ($basename == 'index.php') {
-    $tpl_main = str_replace('<pun_desc>', '<div class="hd_bott">' . $pun_config['o_board_desc'] . '</div>
-', $tpl_main);
+if ($basename == 'index.php' && $pun_config['o_board_desc']) {
+    $tpl_main = str_replace('<pun_desc>', '<div class="hd_bott">' . pun_htmlspecialchars($pun_config['o_board_desc']) . '</div>', $tpl_main);
+} else {
+    $tpl_main = str_replace('<pun_desc>', '', $tpl_main);
 }
 // END SUBST - <pun_desc>
 
@@ -138,7 +137,6 @@ $tpl_main = str_replace('<div id="brdheader" class="block">', '<div>', $tpl_main
 $tpl_main = str_replace('<div class="box">', '<div>', $tpl_main);
 $tpl_main = str_replace('<div id="brdtitle" class="inbox">', '', $tpl_main);
 $tpl_main = str_replace('<pun_js_helper>', '', $tpl_main);
-$tpl_main = str_replace('<pun_hd>', '', $tpl_main);
 $tpl_main = str_replace('<pun_navlinks>', '', $tpl_main);
 // END WAP MOD
 
@@ -151,18 +149,14 @@ $tpl_temp .= '<div class="in"><strong>RSS</strong><div class="box"><div><a href=
 
 // START SUBST - <pun_announcement>
 //Объявление только на главной
-if ($basename == 'index.php') {
-    if ($pun_config['o_announcement'] == 1) {
-        ob_start();
-    
-        echo '
-        <div class="incqbox">' . $lang_common['Announcement'] . '</div>
-        <div class="msg">' . $pun_config['o_announcement_message'] . '</div>';
-    
-        $tpl_temp = trim(ob_get_contents());
-        $tpl_main = str_replace('<pun_announcement>', $tpl_temp, $tpl_main);
-        ob_end_clean();
-    }
+if ($basename == 'index.php' && $pun_config['o_announcement'] == 1) {
+    ob_start();
+
+    echo '<div class="incqbox">' . $lang_common['Announcement'] . '</div><div class="msg">' . $pun_config['o_announcement_message'] . '</div>';
+
+    $tpl_temp = trim(ob_get_contents());
+    $tpl_main = str_replace('<pun_announcement>', $tpl_temp, $tpl_main);
+    ob_end_clean();
 } else {
     $tpl_main = str_replace('<pun_announcement>', '', $tpl_main);
 }
