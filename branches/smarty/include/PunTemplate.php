22177ffddb3aed1e32fce704e06e2e735658f7f9
wapinet <wapinet@efb28ca5-a285-28e4-ec76-7e758185a0da>
2012-04-15 16:38:02 +0000

    del cache files, upd Smarty to 3.1.8

--- branches/smarty/include/PunTemplate.php
+++ branches/smarty/include/PunTemplate.php
@@ -4,11 +4,11 @@ require_once('Smarty/Smarty.class.php');
 
 class PunTemplate extends Smarty
 {
-    function __construct($punDesignName)
+    public function __construct($punDesignName)
     {
         parent::__construct();
         
-        $root = str_replace($_SERVER['DOCUMENT_ROOT'], null, str_replace('\\', '/', dirName(__FILE__))) . '/template/wap/' . $punDesignName . '/';
+        $root = str_replace($_SERVER['DOCUMENT_ROOT'], '', str_replace('\\', '/', dirName(__FILE__))) . '/template/wap/' . $punDesignName . '/';
         
         $punDesignDir = dirName(__FILE__) . '/template/wap/' . $punDesignName;
         
