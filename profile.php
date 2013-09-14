c052bb44bf36039eee9cf94bc5e3367e316e4b3f
Gemorroj <wapinet@gmail.com>
2013-09-15 00:15:05 +0300

    fix Примечание администратора в профиле пользователя

--- profile.php
+++ profile.php
@@ -1418,7 +1418,7 @@ if (isset($_GET['preview']) or ($pun_user['id'] != $id && ($pun_user['g_id'] >
         }
         echo $posts_field . $files_field;
         if ($pun_user['g_id'] < PUN_GUEST) {
-            echo '<label>' . $lang_profile['Admin note'] . '<br /><input id="admin_note" type="text" name="admin_note" value="' . pun_htmlspecialchars($user['admin_note']) . '" size="30" maxlength="30" /><br /></label></div>';
+            echo '<label>' . $lang_profile['Admin note'] . '<br /><input id="admin_note" type="text" name="admin_note" value="' . pun_htmlspecialchars($user['admin_note']) . '" size="30" maxlength="30" /><br /></label>';
         }
         echo '</div></fieldset>
 </div>
