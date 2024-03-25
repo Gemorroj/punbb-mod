f70a6505f5a6f1870d36ec8732f758ba2a4459f7
Gemorroj <wapinet@mail.ru>
2024-03-25 16:14:36 +0300

    build: upd smarty to 5

--- include/PunTemplate.php
+++ include/PunTemplate.php
@@ -29,6 +29,7 @@ class PunTemplate extends Smarty\Smarty
             ->registerPlugin('modifier', 'sprintf', 'sprintf')
             ->registerPlugin('modifier', 'file_exists', 'file_exists')
             ->registerPlugin('modifier', 'range', 'range')
+            ->registerPlugin('modifier', 'ceil', 'ceil')
             ->registerPlugin('modifier', 'uniqid', 'uniqid')
             ->registerPlugin('modifier', 'get_title', 'get_title') // functions.php
             ->registerPlugin('modifier', 'paginate', 'paginate') // functions.php
